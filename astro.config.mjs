43e5f82e5a0c4dce34c8438f37649acf677c0f2c
SofiDev <angiemeow89@gmail.com>
2024-09-30 07:56:29 -0600

    📝 Se agrega metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:/Frida-ai.com',
+  site: 'https://blog.itssofi.dev',
   integrations: [react(), tailwind()]
 });
