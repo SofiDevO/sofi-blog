06b018caeeb5a78c42ab3b2fca521ba1256fa9c7
SofiDev <angiemeow89@gmail.com>
2024-10-04 05:46:18 -0600

    🚀 Patata

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/404.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/404_D0Owe3yu.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/categories/_slug_.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-
-const page = () => import('../../chunks/_slug__D17iFfPU.mjs');
-
-export { page };
