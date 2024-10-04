8d94ffa90006295de3f774fd3a5fa51affefaf3a
SofiDev <angiemeow89@gmail.com>
2024-10-04 05:16:01 -0600

    🚑️ Fix deploy
    Se agrega output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.itssofi.dev',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server'
 });
