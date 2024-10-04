159bb02e0efc3f172ae594bc47656eec11c7f75e
SofiDev <angiemeow89@gmail.com>
2024-10-04 05:43:25 -0600

    🚑️ Fix deploy problem
    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.itssofi.dev',
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel()
+
 });
