29f0be07e94b4887157c290cae030144924b2fe2
SofiDev <angiemeow89@gmail.com>
2024-11-05 17:29:40 -0600

    ♻️ Se agregan server Islands

--- src/services/getPostContent.js
+++ src/services/getPostContent.js
@@ -1,42 +1,87 @@
 import { wpquery } from "@src/data/wordpress";
 export const Postdata = await wpquery({
     query: `
-        query getPostContent {
-          posts {
-            nodes {
-              author {
-                node {
-                  avatar {
-                    url
+       query getPostContent {
+        posts {
+          nodes {
+            author {
+              node {
+                avatar {
+                  url
+                }
+                lastName
+                name
+                firstName
+                description
+                comments {
+                  nodes {
+                    author {
+                      node {
+                        url
+                        name
+                        avatar {
+                          default
+                          url
+                        }
+                      }
+                    }
+                    commentId
+                    content(format: RENDERED)
+                    date
+                    id
+                    replies {
+                      nodes {
+                        author {
+                          node {
+                            url
+                            name
+                            avatar {
+                              default
+                              url
+                            }
+                          }
+                        }
+                        content(format: RENDERED)
+                        date
+                      }
+                    }
                   }
-                  lastName
-                  name
-                  firstName
-                  description
-                }
-              }
-              slug
-              date
-              title
-              content(format: RENDERED)
-              contentType {
-                node {
-                  id
-                }
-              }
-              featuredImage {
-                node {
-                  mediaItemUrl
                 }
               }
             }
+            slug
+            date
+            title
+            content(format: RENDERED)
+            contentType {
+              node {
+                id
+                isComment
+              }
+            }
+            featuredImage {
+              node {
+                mediaItemUrl
+              }
+            }
+            tags {
+              nodes {
+                name
+              }
+            }
+            isComment
           }
-          tags {
-            nodes {
-              name
-            }
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
           }
         }
+        tags {
+          nodes {
+            name
+          }
+        }
+      }
 
         `,
   });
