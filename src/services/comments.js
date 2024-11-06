7f0ddb6f43c632b0cea76839ea811b284292e67b
ElStron <luisstron45@gmail.com>
2024-11-06 01:09:27 -0600

    Se crearon querys para el servicio comments.

---
+++ src/services/comments.js
@@ -0,0 +1,76 @@
+import { wpquery } from "@src/data/wordpress";
+export const getCommentsByPostSlug = async (slug) => {
+    try {
+        const data = await wpquery({
+            query: `
+                query getComments {
+                    postBy(slug: "${slug}") {
+                        comments {
+                            nodes {
+                                content
+                                author {
+                                    node {
+                                        name
+                                        avatar {
+                                            url
+                                        }
+                                    }
+                                }
+                                id
+                                date
+                                parentId
+                            }
+                        }
+                    }
+                }
+            `,
+        });
+
+        if (data.postBy === null) {
+            throw new Error("Post not found");
+        }
+        const filterReplies = (comment) => {
+            return comment.parentId === null
+        }
+        
+        const comments = data.postBy.comments.nodes.filter(filterReplies)
+
+        return comments
+    }
+    catch (error) {
+        return []
+    }
+    
+}
+
+export const getRepliesByCommentId = async (id) => {
+    try {
+        const data = await wpquery({
+            query: `
+                query getRepliesByCommentId {
+                    comment(id: "${id}") {
+                        replies(where: {orderby: COMMENT_DATE}) {
+                            nodes {
+                                content
+                                author {
+                                    node {
+                                        avatar {
+                                            url
+                                        }
+                                        email
+                                        name
+                                    }
+                                }
+                                id
+                            }
+                        }
+                    }
+                }
+            `,
+        });
+        return data.comment.replies.nodes
+    }
+    catch (error) {
+        return []
+    }
+}
