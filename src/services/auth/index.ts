f668a78bf2070d837a765ef30f231d5d58c33f09
SofiDev <angiemeow89@gmail.com>
2024-12-20 02:25:21 -0600

    🚀 Se agregan variables

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -4,6 +4,15 @@ import type { AstroCookies } from "astro";
 
 const { SECRET_KEY, WPGRAPHQL_URL } = import.meta.env
 
+/**
+ * Checks if a user is logged in by verifying the access token stored in cookies.
+ *
+ * @param {AstroCookies} cookies - The cookies object containing the access token.
+ * @returns {boolean | string[]} - Returns false if the user is not logged in or if 
+ * the token is invalid. If the token is valid, returns an array of values from the 
+ * decoded token.
+ */
+
 export function isLoggedIn(cookies: AstroCookies): boolean | string[] {
     const accessToken = cookies.get("accessToken")?.value;
     if (!accessToken ) return false
@@ -16,6 +25,15 @@ export function isLoggedIn(cookies: AstroCookies): boolean | string[] {
     }
 }
 
+
+/**
+ * Verifies if a user is valid by checking the credentials against the WordPress REST API.
+ *
+ * @param {string} user - The username to verify.
+ * @param {string} password - The password to verify.
+ * @returns {Promise<WP_User | null>} - Returns the user data if the credentials are valid, or
+ * null if the credentials are invalid.
+ */
 export async function isValidUser(user: string, password: string) {
     try {
         const wp = new WPAPI({
@@ -29,5 +47,6 @@ export async function isValidUser(user: string, password: string) {
         
     } catch (error) {
         return null;
+        console.error("JWT Verification Error:", error.message);
     }
 }
