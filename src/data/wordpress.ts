6cbe7684f8c3e205b0dc67e0baa529dc074e187c
SofiDev <angiemeow89@gmail.com>
2025-01-02 23:28:49 -0600

    🔀 Reesolving conflics

--- src/data/wordpress.ts
+++ src/data/wordpress.ts
@@ -1,17 +1,18 @@
 const baseURL = `${import.meta.env.WPGRAPHQL_URL}`;
-const { SECRET_USER, SECRET_PASSWORD } = import.meta.env;
+
 interface WPGraphQLParams {
   query?: string;
   variables?: object;
+  headers?: object;
 }
 
-export async function wpquery({ query, variables = {}}: WPGraphQLParams) {
+export async function wpquery({ query, variables = {}, headers = {} }: WPGraphQLParams) {
   
   const res = await fetch(baseURL, {
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      'Authorization': 'Basic ' + btoa(SECRET_USER + ':' + SECRET_PASSWORD)
+      ...headers,
     },
     body: JSON.stringify({
       query,
