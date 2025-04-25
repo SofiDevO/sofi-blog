580515ace7b1cf3e4f080ba72d319cca172ffbb8
SofiDev <angiemeow89@gmail.com>
2025-04-24 19:11:02 -0600

    ♻️ Fixing build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,17 +4,3 @@ export function replaceIframes(html) {
       return `<fast-youtube ${attributes}></fast-youtube>`;
     });
 }
-
-export function dateFormated(date:string){
-
-  const fecha = new Date(date);
-
-  const formatt = fecha.toLocaleDateString('es-ES', {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-
-  return formatt
-}
