85042c6125d5afe5bed52bfb61e036dec61e7e3d
SofiDev <angiemeow89@gmail.com>
2025-07-11 01:42:05 -0600

    💄 Agregando sidebar

---
+++ src/pages/support/contributor/rollList.js
@@ -0,0 +1,25 @@
+export const rollList = [
+  {
+    name: "Frontend Developer",
+    icon: "azure-pipelines",
+    color: "100, 181, 246",
+  },
+  {
+    name: "Backend Developer",
+    icon: "3d",
+    color: "41, 182, 246",
+  },
+  { name: "Diseñador", icon: "figma", color: "124, 77, 255" },
+  { name: "Escritor", icon: "dhall", color: "120, 144, 156" },
+  {
+    name: "Divulgador de programación",
+    icon: "brainfuck",
+    color: "255, 64, 129",
+  },
+  { name: "Javascript Developer", icon: "javascript", color: "255, 202, 40" },
+  { name: "Mobile Developer", icon: "architecture", color: "102, 187, 106" },
+  { name: "Editor de Video", icon: "video", color: "255, 152, 0" },
+  { name: "Maquetador", icon: "palette", color: "79, 195, 247" },
+  { name: "Fullstack Developer", icon: "astyle", color: "239, 83, 80" },
+  { name: "UX/UI", icon: "bazel", color: "129, 199, 132" },
+];
