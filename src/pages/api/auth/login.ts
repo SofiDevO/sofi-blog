fc73eecdf5aa1c42df95a1259c9916463efaf588
SofiDev <angiemeow89@gmail.com>
2025-07-18 00:20:06 -0600

    💄 Se agregan estilos a  descripción

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -10,17 +10,12 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const cookiesExist = cookies.get("accessToken");
 
     if (cookiesExist) return redirect("/dashboard");
-
     const user = (formData.get("username") as string) || undefined;
     const password = (formData.get("password") as string) || undefined;
-
     if (!user || !password) return redirect("/login?error=true&message=Please fill in all fields.");
-
     const data = await isValidUser(user, password);
     if (!data) return redirect("/login?error=true&message=Invalid Credentials.");
-
     const token = Jwt.sign(data, SECRET_KEY, { expiresIn: "1h" });
     cookies.set("accessToken", token, { path: "/", httpOnly: true });
-
     return redirect("/dashboard");
 }
