d527007c1781b302a2c65df97b8a1cc239212c88
ElStron <luisstron45@gmail.com>
2025-07-23 19:17:20 -0600

    refactor: Se refactorizaron los colaboradores y se añadieron sus tipos sin procesar.

--- src/services/getContributor.ts
+++ src/services/getContributor.ts
@@ -1,11 +1,16 @@
 import graphqlReq from "@src/data/wordpresscopy";
-import type { Contributtor } from "@src/types/contributtors.types";
+import type {
+  Contributtor,
+  ContributtorRow,
+} from "@src/types/contributtors.types";
 import type { Post } from "@src/types/post.type";
 import { queryContributor } from "./querys/contributors/contributor";
 
 export async function getContributor(slug: string): Promise<Contributtor> {
   try {
-    const data = await graphqlReq(queryContributor(slug));
+    const data = await graphqlReq<ContributtorRow["data"]>(
+      queryContributor(slug),
+    );
 
     const contributorData = data.contributtor.contribuidores;
     const userConnection = contributorData.tuUsuario?.nodes?.[0];
@@ -16,29 +21,15 @@ export async function getContributor(slug: string): Promise<Contributtor> {
       cv: contributorData.cv?.node?.link,
       banner: contributorData.banner?.node,
       profilepic: contributorData.profilepic?.node,
-      tuUsuario: userConnection
-        ? {
-            ...userConnection,
-            avatar: {
-              url: userConnection.avatar.node?.mediaItemUrl,
-              default: userConnection.avatar.node?.mediaItemUrl || '',
-            },
-            posts: userConnection.posts?.nodes.map((post): Post => {
-              return {
-                ...post,
-                image: post.featuredImage?.node,
-                author: {
-                  ...post.author.node,
-                  slug: userConnection.slug,
-                  avatar: {
-                    url: userConnection.avatar.node?.mediaItemUrl,
-                    default: userConnection.avatar.node?.mediaItemUrl || '',
-                  },
-                },
-              };
-            }) || [],
-          }
-        : undefined,
+      posts:
+        userConnection.posts?.nodes.map((post): Post => {
+          return {
+            ...post,
+            image: post.featuredImage?.node,
+          };
+        }) || [],
+      slug: data.contributtor.slug,
+      author: userConnection.posts?.nodes[0]?.author.node || {},
     };
 
     return authorData;
--- src/types/contributtors.types.ts
+++ src/types/contributtors.types.ts
@@ -1,4 +1,5 @@
-import  type { User } from "./user.type";
+import type { Author } from "./author.type";
+import type { Post } from "./post.type";
 export interface Contributtor {
   banner: {
     altText: string;
@@ -6,11 +7,11 @@ export interface Contributtor {
     sizes: string;
     srcSet: string;
   };
-  customcolor: string;
-  cv: string;
-  description: string;
-  email: string;
-  embedVideo: string;
+  customcolor?: string;
+  cv?: string;
+  description?: string;
+  email?: string;
+  embedVideo?: string;
   name: string;
   rol: string[];
   profilepic: {
@@ -19,18 +20,103 @@ export interface Contributtor {
     sizes: string;
     srcSet: string;
   };
-  socialLinks: {
-    github: string;
-    instagram: string;
-    kofi: string;
-    linkedin: string;
-    twitch: string;
-    youtube: string;
-  };
+  socialLinks: SocialLinks;
   slug: string;
-  tuUsuario?: User;
-
-
+  posts: Post[];
+  author: Author;
 }
 
-export type CardContributtor = Omit<Contributtor, "cv" | "embedVideo" |  "description" | "tuUsuario">
+export type CardContributtor = Omit<
+  Contributtor,
+  "cv" | "embedVideo" | "description" | "tuUsuario"
+>;
+
+// Row Contributtor
+export interface ContributtorRow {
+  data: Data;
+  extensions: Extensions;
+}
+
+export interface Data {
+  contributtor: Contributtor2;
+}
+
+export interface Contributtor2 {
+  socialLinks: SocialLinks;
+  title: string;
+  contribuidores: Contribuidores;
+  slug: string;
+}
+
+export interface SocialLinks {
+  github: string;
+  instagram: any;
+  koFi: string;
+  linkedin: string;
+  twitch: string;
+  youtube: string;
+}
+
+export interface Contribuidores {
+  banner: Banner;
+  customcolor: string;
+  cv: any;
+  description: string;
+  email: string;
+  name: string;
+  profilepic: Profilepic;
+  rol: string[];
+  tuUsuario: TuUsuario;
+}
+
+export interface Banner {
+  node: Node;
+}
+
+export interface Node {
+  altText: string;
+  mediaItemUrl: string;
+  sizes: string;
+  srcSet: string;
+  authorDatabaseId: number;
+}
+
+export interface Profilepic {
+  node: Node2;
+}
+
+export interface Node2 {
+  altText: string;
+  mediaItemUrl: string;
+  srcSet: any;
+  sizes: string;
+}
+
+export interface TuUsuario {
+  nodes: Node3[];
+}
+
+export interface Node3 {
+  username: string;
+  posts: Posts;
+  avatar: Avatar;
+}
+
+export interface Posts {
+  nodes: any[];
+}
+
+export interface Avatar {
+  url: string;
+  default: string;
+}
+
+export interface Extensions {
+  debug: Debug[];
+}
+
+export interface Debug {
+  type: string;
+  message: string;
+}
+
