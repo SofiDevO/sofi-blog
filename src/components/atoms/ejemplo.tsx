609aa42d05caf193065b914b794323b0cf7dd553
SofiDev <angiemeow89@gmail.com>
2025-07-25 23:40:26 -0600

    🚧 Refactorizando el Home del blog
    refactorizando el Home del blog

---
+++ src/components/atoms/ejemplo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const ejemplo = () => {
+  let traducir = "Que pasa mundo";
+    return (
+    <div>
+      <h1>{traducir}</h1>
+      <p>Este es un texto de ejemplo para probar la traducción en español</p>
+    </div>
+  )
+}
+
+export default ejemplo
---
+++ src/translations/translations.types.ts
@@ -0,0 +1,9 @@
+export interface Translations {
+    "Hola mundo": string;
+    "Este es un texto de ejemplo para probar la rtranslación en español": {
+        "": string;
+    };
+    "Hola mundo de neuvo": string;
+    "Como se encuentran el día de hoy": string;
+    "Que pasa mundo": string;
+}
