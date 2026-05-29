3a3497ed6bf32cc5b2cb41aee526a2ae0c51cfcd
SofiDev <angiemeow89@gmail.com>
2026-05-29 15:21:33 -0600

    ✨ Feat: introudce Module detail

---
+++ src/services/querys/modules/getModuleDetail.ts
@@ -0,0 +1,37 @@
+import { wpquery } from "@src/data/wordpress";
+import type { ModuleDetailResponse, ModuleNode } from "@src/types/moduleDetail.type";
+
+export const getModuleDetail = async (): Promise<ModuleNode[]> => {
+  const data = await wpquery<ModuleDetailResponse>({
+    query: `
+      query getModuleDetail {
+        modules {
+          nodes {
+            databaseId
+            slug
+            title
+            moduleFields {
+              recursos {
+                node {
+                  mediaItemUrl
+                }
+              }
+            }
+            content(format: RENDERED)
+            date
+            featuredImage {
+              node {
+                altText
+                mediaItemUrl
+                sizes(size: MEDIUM)
+                srcSet(size: MEDIUM)
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  return data?.modules?.nodes ?? [];
+};
